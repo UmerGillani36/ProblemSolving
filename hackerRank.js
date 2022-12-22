9f91318a87e1d05eefc7a63f6d6ba75f0f21572d
UmerGillani36 <syedomershahgillani.com>
2022-12-22 02:27:13 -0800

    hackerRank in a string done

--- hackerRank.js
+++ hackerRank.js
@@ -1,44 +1,16 @@
 function hackerrankInString(s) {
     let hackerRank='hackerrank';
-    let arr=[];
-    let val;
-    let flag=false;
-    for(let char of hackerRank){
+    let iterator=0;
         for(let i=0;i<s.length;i++){
-            if(char===s[i]){
-                flag=true;
-                if(char==='a'){
-                    arr.push(i);
-                }else{
-                    val=i;
-                }
-            }else{
-                flag=false;
+            if(hackerRank[iterator] === s[i]){
+                iterator++;
             }
         }
-        if(flag=false){
-            return 'NO';
-        }else{
-            arr.push(val);
-        }
+    if(hackerRank.length===iterator){
+        return 'YES';
+    }else{
+        return 'NO';
     }
-    let len=arr.length;
-    console.log('Array',arr);
-    let flag1=false;
- if(len<10){
-     return 'NO';
- }
- for(let i=0; i<len; i++){
-     if(arr[i]>arr[i+1]){
-         return 'NO'
-     }else{
-         flag1=true;
-     }
- }
- if(flag1){
-     return 'YES';
- }
-
 }
 
 let result=hackerrankInString('hereiamstackerrank');
