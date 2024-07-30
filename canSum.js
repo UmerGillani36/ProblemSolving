2e0bdd30b713d8d71e537389eeccd19dd1e5e2f6
umergillani36 <syedomershahgillani@gmail.com>
2024-07-30 16:49:08 +0500

    canSum

---
+++ canSum.js
@@ -0,0 +1,36 @@
+// Traditional Recursion
+// const canSum = (targetSum, numbers) => {
+//   if (targetSum === 0) return true;
+//   if (targetSum < 0) return false;
+
+//   for (let num of numbers) {
+//     const reminder = targetSum - num;
+//     if (canSum(reminder, numbers) === true) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// Memoization (Dynamic Programming)
+
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (let num of numbers) {
+    const reminder = targetSum - num;
+    if (canSum(reminder, numbers, memo) === true) {
+      memo[targetSum] = true;
+      return true;
+    }
+  }
+  memo[targetSum] = false;
+  return false;
+};
+console.log(canSum(7, [2, 3])); // true
+console.log(canSum(7, [5, 3, 4, 7])); // true
+console.log(canSum(7, [2, 4])); // false
+console.log(canSum(8, [2, 3, 5])); // true
+console.log(canSum(300, [7, 14])); // false
