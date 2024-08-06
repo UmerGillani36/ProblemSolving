04bc46e3006177bf6eb2eca01288ae52d635b44a
umergillani36 <syedomershahgillani@gmail.com>
2024-08-06 12:11:26 +0500

    bestSum ( Dynamic Programming )

---
+++ bestSum.js
@@ -0,0 +1,29 @@
+const bestSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  let shortestCombination = null;
+
+  for (let num of numbers) {
+    const reminder = targetSum - num;
+    const reminderCombination = bestSum(reminder, numbers, memo);
+
+    if (reminderCombination != null) {
+      const combination = [...reminderCombination, num];
+      if (
+        shortestCombination === null ||
+        combination.length < shortestCombination.length
+      ) {
+        shortestCombination = combination;
+      }
+    }
+  }
+  memo[targetSum] = shortestCombination;
+  return shortestCombination;
+};
+
+console.log(bestSum(7, [5, 3, 4, 7])); // [7]
+console.log(bestSum(8, [2, 3, 5])); // [3, 5]
+console.log(bestSum(8, [1, 4, 5])); // [4,4]
+console.log(bestSum(100, [1, 2, 5, 25])); // [25,25,25,25]
