011628cbafa23b2afea8a43017da83be4edd4c31
umergillani36 <syedomershahgillani@gmail.com>
2024-08-12 13:05:38 +0500

    fib (DP-tabulation)

---
+++ fib.js
@@ -0,0 +1,17 @@
+// DP (Tabulation Method)
+// O(n) time & space
+
+const fib = (n) => {
+  const table = Array(n + 1).fill(0);
+  table[1] = 1;
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
+  }
+  return table[n];
+};
+
+console.log(fib(6)); // 8
+console.log(fib(7)); // 13
+console.log(fib(8)); // 21
+console.log(fib(50)); // 1258269025
