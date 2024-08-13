ff15e3fd9fdb95a4b30fed827113a8e5489f3ecd
umergillani36 <syedomershahgillani@gmail.com>
2024-08-13 16:46:54 +0500

    gridTraveler (Memoization)

--- gridTraveler.js
+++ gridTraveler.js
@@ -10,34 +10,34 @@
 //      Dynamic Programming
 //            1) Memoization
 
-// const gridTraveler = (m, n, memo = {}) => {
-//   const key = m + "," + n;
-//   if (key in memo) return memo[key];
-//   if (m === 1 && n === 1) return 1;
-//   if (m === 0 || n === 0) return 0;
+const gridTraveler = (m, n, memo = {}) => {
+  const key = m + "," + n;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
 
-//   memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
-//   return memo[key];
-// };
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+  return memo[key];
+};
 
 //            1) Tabulation
-const gridTraveler = (m, n) => {
-  const table = Array(m + 1)
-    .fill()
-    .map(() => Array(n + 1).fill(0));
+// const gridTraveler = (m, n) => {
+//   const table = Array(m + 1)
+//     .fill()
+//     .map(() => Array(n + 1).fill(0));
 
-  table[1][1] = 1;
+//   table[1][1] = 1;
 
-  for (let i = 0; i <= m; i++) {
-    for (let j = 0; j <= n; j++) {
-      const current = table[i][j];
-      if (j + 1 <= n) table[i][j + 1] += current; // right
-      if (i + 1 <= m) table[i + 1][j] += current; // bottom
-    }
-  }
+//   for (let i = 0; i <= m; i++) {
+//     for (let j = 0; j <= n; j++) {
+//       const current = table[i][j];
+//       if (j + 1 <= n) table[i][j + 1] += current; // right
+//       if (i + 1 <= m) table[i + 1][j] += current; // bottom
+//     }
+//   }
 
-  return table[m][n];
-};
+//   return table[m][n];
+// };
 
 console.log(gridTraveler(1, 1)); // 1
 console.log(gridTraveler(2, 3)); // 3
