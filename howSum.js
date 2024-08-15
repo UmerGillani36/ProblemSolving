4a4c3b5377cde44d38bc8eabaebe4d37bf4ffe5a
umergillani36 <syedomershahgillani@gmail.com>
2024-08-15 12:25:13 +0500

    Tabulation (Dynamic Programming)

--- howSum.js
+++ howSum.js
@@ -1,26 +1,43 @@
-const howSum = (targetSum, numbers, memo = {}) => {
-  if (targetSum in memo) return memo[targetSum];
-  if (targetSum === 0) return [];
-  if (targetSum < 0) return null;
+// Memoization (Dynamic Programming)
 
-  for (let num of numbers) {
-    const reminder = targetSum - num;
-    const reminderResult = howSum(reminder, numbers, memo);
+// const howSum = (targetSum, numbers, memo = {}) => {
+//   if (targetSum in memo) return memo[targetSum];
+//   if (targetSum === 0) return [];
+//   if (targetSum < 0) return null;
 
-    if (reminderResult !== null) {
-      memo[targetSum] = [...reminderResult, num];
+//   for (let num of numbers) {
+//     const reminder = targetSum - num;
+//     const reminderResult = howSum(reminder, numbers, memo);
 
-      return memo[targetSum];
+//     if (reminderResult !== null) {
+//       memo[targetSum] = [...reminderResult, num];
+
+//       return memo[targetSum];
+//     }
+//   }
+
+//   memo[targetSum] = null;
+
+//   return null;
+// };
+
+// Tabulation (Dynamic Programming)
+
+const howSum = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(null);
+  table[0] = [];
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] !== null) {
+      for (let num of numbers) {
+        table[i + num] = [...table[i], num];
+      }
     }
   }
-
-  memo[targetSum] = null;
-
-  return null;
+  return table[targetSum];
 };
 
-console.log(howSum(7, [2, 3]));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(7, [2, 4]));
-console.log(howSum(8, [2, 3, 5]));
-console.log(howSum(300, [7, 14]));
+console.log(howSum(7, [2, 3])); // [ 3, 2, 2 ]
+console.log(howSum(7, [5, 3, 4, 7])); // [ 4, 3 ]
+console.log(howSum(7, [2, 4])); // null
+console.log(howSum(8, [2, 3, 5])); // [ 2, 2, 2, 2 ]
+console.log(howSum(300, [7, 14])); // null
